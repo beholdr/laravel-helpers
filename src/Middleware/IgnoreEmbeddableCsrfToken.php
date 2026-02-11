b0f31a3a01d6985bb6559bdf1d12800af9f71e03
Alexander Shabunevich <loonar@gmail.com>
2026-02-11 12:27:35 +0300

    chore: livewire 4 support

--- src/Middleware/IgnoreEmbeddableCsrfToken.php
+++ src/Middleware/IgnoreEmbeddableCsrfToken.php
@@ -44,12 +44,23 @@ class IgnoreEmbeddableCsrfToken extends VerifyCsrfToken
     private function isEmbeddable($component): bool
     {
         try {
-            $reflectionClass = new ReflectionClass(app(ComponentRegistry::class)->new($component));
+            $reflectionClass = new ReflectionClass($this->getClass($component));
             $embedAttribute = $reflectionClass->getAttributes(Embeddable::class)[0] ?? null;
 
             return is_null($embedAttribute) === false;
         } catch (ComponentNotFoundException) {
             return false;
         }
+    }
+
+    private function getClass($component)
+    {
+        if (app()->has(ComponentRegistry::class)) {
+            return app(ComponentRegistry::class)->new($component);
+        }
+
+        $className = app('livewire.finder')->resolveClassComponentClassName($component);
+
+        return app($className);
     }
 }
